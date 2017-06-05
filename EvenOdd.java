11ee7c15e5ac948d574e35c8c9c2b01b07592398
Amuthasathya Rajendiran <amuthasathyaa@gmail.com>
2017-06-05 11:59:40 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+class EvenOdd
+{
+	public static void main(String arg[])
+	{
+		int a=Integer.parseInt(arg[0]);
+		if(a%2==0)
+		{
+			System.out.println("Even");
+		}
+		else
+		{
+			System.out.println("Odd");
+		}
+	}
+}
+	
