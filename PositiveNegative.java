3a63ae0d5c29107ce228acd53cee520268b7dbb4
Amuthasathya Rajendiran <amuthasathyaa@gmail.com>
2017-06-05 11:56:25 +0530

    Create PositiveNegative.java

---
+++ PositiveNegative.java
@@ -0,0 +1,21 @@
+# Guvi-Programs
+class CheckSigns
+{
+	public static void main(String arg[])
+	{
+		int a=Integer.parseInt(arg[0]);
+		if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else if(a<0)
+		{
+			System.out.println("Negative");
+		}
+		else	
+		{
+			System.out.println("Zero");
+		}
+	}
+}
+		
